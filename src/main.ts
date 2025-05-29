1801ce575ab2b60564e203e6bfe7167be0fa0388
GrimmFStack <luisalfonsoekpech@gmail.com>
2025-05-29 12:52:51 -0600

    Add .gitignore file and update @types/node version to 22.15.24; configure app to use environment port

--- src/main.ts
+++ src/main.ts
@@ -22,6 +22,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  console.log(`App running on port ${port}`);
 }
 bootstrap();
